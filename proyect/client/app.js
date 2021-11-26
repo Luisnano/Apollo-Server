25d830bb352801f515d346fbe216b67d7490fa7c
Kali <luis.torresmo@alum.uca.es>
2021-11-26 12:55:57 +0000

    Probando los Fetch de un user

--- proyect/client/app.js
+++ proyect/client/app.js
@@ -19,7 +19,88 @@ async function fetchGreeting() {
   return data;
 }
 
+/*Lets create a fetch for the user we created in the server and try it out*/
+
+
+/*First of all, lest fetch the username*/
+async function fetchFirst_Name() {
+    const response = await fetch(GRAPHQL_URL, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          query {
+            first_name
+          }
+        `,
+      }),
+    });
+  
+    const { data } = await response.json();
+    return data;
+  }
+
+
+
+/*Lets continue eith the last_name*/
+async function fetchLast_Name() {
+    const response = await fetch(GRAPHQL_URL, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          User {
+            last_name
+          }
+        `,
+      }),
+    });
+  
+    const { data } = await response.json();
+    return data;
+  }
+
+
+
+
+/*Last but not least, the id*/
+async function fetchId() {
+    const response = await fetch(GRAPHQL_URL, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          User {
+            id
+          }
+        `,
+      }),
+    });
+  
+    const { data } = await response.json();
+    return data;
+  }
+
+
 fetchGreeting().then(({ greeting }) => {
   const title = document.querySelector('h1');
   title.textContent = greeting;
 });
+
+fetchFirst_Name().then(({ first_name }) => {
+    const title = document.querySelector('h2');
+    title.textContent = first_name;
+  }).then(fetchLast_Name().then(({ last_name }) => {
+    const title = document.querySelector('h3');
+    title.textContent = last_name;
+  })).then(fetchId().then(({ id }) => {
+    const title = document.querySelector('h4');
+    title.textContent = id;
+  }));
+
--- proyect/server/server.js
+++ proyect/server/server.js
@@ -1,15 +1,28 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+
+/*Lets try to create a User type and displaying it on de web api*/
 const typeDefs = gql`
   type Query {
     greeting: String
+  },
+  type Query {
+    first_name: String,
+    last_name: String,
+    id: String
   }
 `;
+/*Lets try to create a User type and displaying it on de web api*/
 
 const resolvers = {
   Query: {
     greeting: () => 'Hello GraphQL world!👋',
   },
+  Query:{
+    first_name: () => 'Luis Fernando',
+    last_name: () => 'Torres Moya',
+    id: () => '32093709-S'
+  }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
